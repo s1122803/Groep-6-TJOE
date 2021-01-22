170db962ece432d0a265a72d5e5c44d2037b86a9
Tycho van Veen <s1122689@student.hsleiden.nl>
2021-01-22 12:21:21 +0100

    added sound

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 window.onload = () => {
 	const startButton = document.getElementById('startButton');
+	const optiesButton = document.getElementById('optiesButton');
 	const menuScene = document.getElementById('js--menuScene');
 	const winkelScene = document.getElementById('js--winkelScene');
 
+	optiesButton.addEventListener('click', function () {
+		menuScene.setAttribute('visible', 'false');
+		winkelScene.setAttribute('visible', 'true');
+	});
 	startButton.addEventListener('click', function () {
 		menuScene.setAttribute('visible', 'false');
 		winkelScene.setAttribute('visible', 'true');
