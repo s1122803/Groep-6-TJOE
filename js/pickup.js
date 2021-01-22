26de5178cd623aaf7ab67fbdd0cc79a23612432e
KoenSmit <ksp.smit@gmail.com>
2021-01-22 15:53:37 +0100

    pickup component

---
+++ js/pickup.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent("pickup", {
+    init:function(){
+        this.el.addEventListener('click', function (evt) {
+            console.log("test");
+        });
+    },
+    update:function(){
+ 
+    },
+    tick:function(){
+ 
+    },
+    remove:function(){
+
+    },
+    pause:function(){
+
+    },
+    play:function(){
+
+    }
+});
