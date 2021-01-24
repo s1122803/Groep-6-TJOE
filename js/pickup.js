4a84435d73e2c646080f8ba561b36636cee1acb9
KoenSmit <ksp.smit@gmail.com>
2021-01-24 17:27:40 +0100

    Added checkmarks on phone for picked up products

--- js/pickup.js
+++ js/pickup.js
@@ -1,42 +1,45 @@
 AFRAME.registerComponent("pickup", {
     init:function(){
         const phoneList = document.getElementById('js--phone-shoppinglist');
-	    const phone = document.getElementById('js--phone');
-        let pickups = document.getElementsByClassName('js--pickup');
-        let itemInArray;
+        const checkmark = document.getElementsByClassName('js--checkmark');
         let executeFix = 0;
+
+        const updateShoppingList = (itemName) => {
+            let list;
+            for (let i = 0; i < updatableList.length; i++) {
+                if(updatableList[i] == itemName){
+                    checkmark[i].setAttribute('color', 'green');
+                }
+                if(i === 0){
+                    list = updatableList[0] + '\n';
+                }
+                else{
+                    list = list + updatableList[i] + '\n';
+                }
+            }
+            phoneList.setAttribute('value', list);
+        }
+
         this.el.addEventListener('click', function () {
             let itemId = this.getAttribute('class').split(' ');
             if(executeFix === 0){
                 executeFix = 1;
                 for(let i = 0; i<updatableList.length; i++){
                     if(itemId[1] == updatableList[i]){
-                        itemInArray = i;
                         this.setAttribute('animation', 'property: scale; to: 0 0 0; dur: 500; easing: linear; loop: false')
                         setTimeout(function(){
-                            this.remove();
                             executeFix = 0;
+                            updateShoppingList(itemId[1]);
+                            this.remove();
+                      
+                            
                         },510);
                     }  
                 }
             }
             
-            return
-            function updateShoppingList() {
-                let list;
-                for (let i = 0; i < updatableList.length; i++) {
-
-                    if(i === 0){
-                        list = updatableList[0] + '\n';
-                    }
-                    else{
-                        list = list + updatableList[i] + '\n';
-                    }
-                }
-                phoneList.setAttribute('value', list);
             
-            }
-            updateShoppingList();
+            
         });
     },
     update:function(){
