26b61b2ea2326129af86dc6ae333478229e90c8c
Tycho van Veen <s1122689@student.hsleiden.nl>
2021-01-27 15:00:10 +0100

    Teleporteren werkend gemaakt

--- js/start-game.js
+++ js/start-game.js
@@ -2,10 +2,13 @@ AFRAME.registerComponent('startbutton', {
 	init: function () {
 		const menuScene = document.getElementById('js--menuScene');
 		const winkelScene = document.getElementById('js--winkelScene');
+		const camera1 = document.getElementById('head1');
+		const lefthand = document.getElementById('left-hand1');
 		this.el.addEventListener('click', function () {
-			console.log('log');
 			menuScene.setAttribute('visible', 'false');
 			winkelScene.setAttribute('visible', 'true');
+			camera1.setAttribute('raycaster', 'objects: .clickable; far: 1;');
+			lefthand.setAttribute('teleport-controls', 'cameraRig: #cameraRig1; teleportOrigin: #head1; button: trigger; collisionEntities: .js--welLopen; curveShootingSpeed:5;');
 		});
 	},
 	update: function () {},
