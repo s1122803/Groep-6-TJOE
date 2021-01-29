be25aa018646928f93ce2fd61612da84f4b6b6b5
Vincent Knijff <s1122803@student.hsleiden.nl>
2021-01-29 11:24:26 +0100

    laatste product toegevoegd en lege bestanden weggehaald

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Global variables
 
 // Alle items en de prijzen ervan (Prijzen van de items moeten op dezeflde positie in de array staan)
-const shopItemList = ['appel', 'banaan', 'peer', 'broccoli', 'sla', 'paprika', 'brood', 'kip', 'gehakt', 'spaghetti', 'pastasaus', 'pringles', 'chocolade', 'chips-naturel', 'chips-paprika', 'mais', 'cola', 'fanta', 'water', 'sinaasappelsap', 'melk', 'vla', 'yoghurt', 'boter', 'kaas', 'salami'];
-const shopPriceList = [0.60, 0.99, 0.60, 1.49, 0.99, 0.89, 1.99, 6.69, 3.09, 1.35, 2.99, 2.09, 2.89, 0.99, 0.99, 1.59, 1.85, 1.99, 0.59, 1.43, 1.45, 1.09, 0.95, 1.39, 2.39, 1.29];
+const shopItemList = ['appel', 'banaan', 'peer', 'broccoli', 'sla', 'paprika', 'brood', 'kip', 'gehakt', 'spaghetti', 'pastasaus', 'pringles', 'chocolade', 'chips-naturel', 'chips-paprika', 'mais', 'cola', 'fanta', 'water', 'sinaasappelsap', 'melk', 'vla', 'yoghurt', 'boter', 'kaas', 'salami', 'spinazie'];
+const shopPriceList = [0.60, 0.99, 0.60, 1.49, 0.99, 0.89, 1.99, 6.69, 3.09, 1.35, 2.99, 2.09, 2.89, 0.99, 0.99, 1.59, 1.85, 1.99, 0.59, 1.43, 1.45, 1.09, 0.95, 1.39, 2.39, 1.29, 1.49];
 let shoppingCartArray = ['', '', '', '', '', '', '', ''];
 let updatableList;
 let rawList = shopItemList;
